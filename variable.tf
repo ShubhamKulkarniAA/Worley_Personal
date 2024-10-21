76c2776eb05a920d7ac04b09336c3f39517a2284
ShubhamKulkarniAA <shubham.kulkarni@alignedautomation.com>
2024-10-21 14:46:14 +0530

    ALB

--- variable.tf
+++ variable.tf
@@ -51,22 +51,6 @@ variable "public_alb_name" {
   type = string
 }
 
-variable "vpc_id" {
-  type = string
-}
-
-variable "public_subnet1" {
-  type = string
-}
-
-variable "public_subnet2" {
-  type = string
-}
-
-variable "public_subnets" {
-  type = list(string)
-}
-
 variable "certificate_arn" {
   description = "ARN of the SSL certificate in AWS Certificate Manager"
   type        = string
