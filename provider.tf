3acca550d043a67913e59c78dedaca1dc3fae5da
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-10-21 23:00:06 +0530

    added dockerfile

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
 
   backend "s3" {
-    bucket = "terraform-be-worley-test"
+    bucket = "demo-terraform-be-worley-test"
     key = "terraform-be-worley-test/terraform.tfstate"
     region = "ap-south-1"
   }
