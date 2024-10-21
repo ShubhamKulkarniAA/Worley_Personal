e0355af8098f4e6054e4a32cbff4f627b9c088ec
jadhavamey <ameyjadhaav@gmail.com>
2024-10-21 11:45:11 +0530

    changes in eks module

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -1,11 +1,11 @@
 output "eks_cluster_name" {
-  value = module.eks.cluster_name
+  value = aws_eks_cluster.cluster.name
 }
 
 output "eks_cluster_endpoint" {
-  value = module.eks.cluster.endpoint
+  value = aws_eks_cluster.cluster.endpoint
 }
 
 output "eks_cluster_ca_certificate" {
-  value = module.eks.cluster.certificate_authority[0].data
+  value = aws_eks_cluster.cluster.certificate_authority
 }
