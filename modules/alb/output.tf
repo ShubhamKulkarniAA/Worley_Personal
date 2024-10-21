fefe82c625f525a80af440d55680411bd7dff010
ShubhamKulkarniAA <shubham.kulkarni@alignedautomation.com>
2024-10-21 14:57:56 +0530

    ALB

--- modules/alb/output.tf
+++ modules/alb/output.tf
@@ -1,9 +1,9 @@
 output "public_alb_arn" {
-  value = module.alb.public_alb_arn
+  value = aws_lb.alb.public_alb_arn
 }
 
-output "Public_alb_tg_arn" {
-  value = module.alb.public_alb_tg_arn
+output "public_alb_tg_arn" {
+  value = aws_lb_target_group.public_alb_tg.arn
 }
 
 /*output "private_alb_arn" {
