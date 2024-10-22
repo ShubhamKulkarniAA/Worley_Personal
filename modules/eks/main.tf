3aa1d055880a8f7b8553545b3d90d7e07471bee9
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-10-22 14:33:33 +0530

    changes in eks-main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -7,7 +7,7 @@ resource "aws_iam_role" "eks_cluster_role" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          Service = "*"
+          Service = "eks.amazonaws.com"
         }
       },
     ]
@@ -38,7 +38,7 @@ resource "aws_iam_role" "eks_node_group_role" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          Service = "*"
+          Service = "eks.amazonaws.com"
         }
       },
     ]
