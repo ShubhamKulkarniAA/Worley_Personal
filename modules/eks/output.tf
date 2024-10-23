93687e448dea44d324a2ab1c6e5324835e8a3ea6
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-10-23 15:59:21 +0530

    adding ECR

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -10,25 +10,8 @@ output "eks_cluster_ca_certificate" {
   value = aws_eks_cluster.cluster.certificate_authority[0].data
 }
 
-output "alb_ingress_policy_arn" {
-  value = aws_iam_policy.alb_ingress_policy.arn
-}
-
-output "eks_node_group_role_arn" {
-  value = aws_iam_role.eks_node_group_role.arn
-}
-
-output "alb_ingress_role_policy_attachment_arn" {
-  value = aws_iam_role_policy_attachment.alb_ingress_role_policy_attachment.arn
-}
-
 # Output for ECR repository URL
 
 output "ecr_repository_url" {
   value = aws_ecr_repository.app1.repository_url
-}
-
-output "ecr_access_policy_arn" {
-  description = "The ARN of the ECR access policy for the EKS node group"
-  value       = aws_iam_policy.ecr_access_policy.arn
 }
