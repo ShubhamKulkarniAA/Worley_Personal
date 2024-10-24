6e7c12d5c1b8e52968bb756647efefd88039033d
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-10-24 14:51:38 +0530

    removing errors

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -45,23 +45,3 @@ variable "private_rds_subnet2_cidr" {
 variable "region" {
   type = string
 }
-
-variable "cluster_name" {
-  description = "EKS Cluster name"
-  type        = string
-}
-
-variable "node_group_name" {
-  description = "EKS Node Group name"
-  type        = string
-}
-
-variable "alb_ingress_role_name" {
-  description = "IAM role name for the ALB Ingress Controller"
-  type        = string
-}
-
-variable "ecr_repository_name" {
-  description = "Name of the ECR repository"
-  type        = string
-}
