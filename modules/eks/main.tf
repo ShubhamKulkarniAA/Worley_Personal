4b0805383296d5b8796f7a20ce72dbf6f7ac593a
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-10-24 12:25:26 +0530

    removing errors

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "cluster_role" {
+resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.cluster_name}-eks-cluster-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -14,7 +14,7 @@ resource "aws_iam_role" "cluster_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "cluster_policy" {
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_cluster_role.name
 }
