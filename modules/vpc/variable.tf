7ad6e1b4c7e545ff284ae9ed0602ea6392d462d5
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-10-24 14:15:09 +0530

    removing errors

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -1,49 +1,68 @@
-variable vpc_cidr {
+variable "vpc_cidr" {
   type = string
 }
 
-variable vpc_name {
+variable "vpc_name" {
   type = string
 }
 
-variable internet_gateway_name {
- type = string 
-}
-
-
-variable public_subnet1_cidr {
+variable "internet_gateway_name" {
   type = string
 }
 
-variable availability_zone1 {
+variable "public_subnet1_cidr" {
   type = string
 }
 
-variable public_subnet2_cidr {
+variable "availability_zone1" {
   type = string
 }
 
-variable availability_zone2 {
+variable "public_subnet2_cidr" {
   type = string
 }
 
-variable private_subnet1_cidr {
+variable "availability_zone2" {
   type = string
 }
 
-variable private_subnet2_cidr {
+variable "private_subnet1_cidr" {
   type = string
 }
 
-
-variable private_rds_subnet1_cidr {
+variable "private_subnet2_cidr" {
   type = string
 }
 
-variable private_rds_subnet2_cidr {
+variable "private_rds_subnet1_cidr" {
+  type = string
+}
+
+variable "private_rds_subnet2_cidr" {
   type = string
 }
 
 variable "region" {
   type = string
+}
+
+# Add the missing variables here
+variable "cluster_name" {
+  description = "EKS Cluster name"
+  type        = string
+}
+
+variable "node_group_name" {
+  description = "EKS Node Group name"
+  type        = string
+}
+
+variable "alb_ingress_role_name" {
+  description = "IAM role name for the ALB Ingress Controller"
+  type        = string
+}
+
+variable "ecr_repository_name" {
+  description = "Name of the ECR repository"
+  type        = string
 }
