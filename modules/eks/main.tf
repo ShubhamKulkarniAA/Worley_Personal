e1bf9d3a8f0baafc84c73e9f700bab813a284380
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-10-25 12:44:31 +0530

    eks

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -69,11 +69,11 @@ resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = var.node_group_name
   node_role_arn   = aws_iam_role.eks_node_group_role.arn
-  subnet_ids      = [var.private_subnet1,var.private_subnet2]
+  subnet_ids      = [var.public_subnet1,var.public_subnet2]
 
   scaling_config {
     desired_size = 1
-    max_size     = 2
+    max_size     = 3
     min_size     = 1
   }
 
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -38,7 +38,7 @@ resource "aws_subnet" "private_subnet1" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = var.private_subnet1_cidr
   availability_zone = var.availability_zone1
-  map_public_ip_on_launch = true
+
   tags = {
     Name = "${var.vpc_name}-PrivateSubnet1"
     "kubernetes.io/cluster/demo"      = "owned"
@@ -49,7 +49,7 @@ resource "aws_subnet" "private_subnet2" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = var.private_subnet2_cidr
   availability_zone = var.availability_zone2
-  map_public_ip_on_launch = true
+
   tags = {
     Name = "${var.vpc_name}-PrivateSubnet2"
     "kubernetes.io/cluster/demo"      = "owned"
