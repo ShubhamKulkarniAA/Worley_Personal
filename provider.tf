b1d41553ff24ff8be86d0f750e8b61417de42213
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-10-28 22:45:21 +0530

    ECR

--- provider.tf
+++ provider.tf
@@ -2,21 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"  # or a specific version like "5.73.0"
+      version = "~> 5.73.0"  # or a specific version like "5.73.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "demo-terraform-be-worley-test"
+    key    = "demo-terraform-be-worley-test/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
 
 provider "aws" {
-  region     = "ap-south-1"
-}
-
-
-terraform {
-
-  backend "s3" {
-    bucket = "demo-terraform-be-worley-test"
-    key = "demo-terraform-be-worley-test/terraform.tfstate"
-    region = "ap-south-1"
-  }
+  region = "ap-south-1"
 }
