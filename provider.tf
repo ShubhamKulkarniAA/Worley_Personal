d7996ab53d905600ac3e09ec8dbc4e783623d420
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-11-04 12:11:20 +0530

    adding LBC

--- provider.tf
+++ provider.tf
@@ -34,6 +34,4 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
 
-  # Local configuration fallback
-  config_path = "~/.kube/config"
 }
