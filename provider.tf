2dd2281368d20d345f83f08a6a3b84d8628625e2
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-11-08 02:29:24 +0530

    ALB Controller

--- provider.tf
+++ provider.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.0"
     }
+    eks = {
+      source  = "hashicorp/eks"
+      version = "~> 1.0"  # Adjust version if needed
+    }
   }
 }
 
@@ -19,4 +23,8 @@ provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
+}
+
+provider "eks" {
+  region = var.region
 }
