d73b88024462dc3bcf67151b1bb9eea41c77467e
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-11-08 16:20:02 +0530

    service account added

--- modules/aws-lbc/helm.tf
+++ modules/aws-lbc/helm.tf
@@ -1,3 +1,13 @@
+resource "kubernetes_service_account" "aws_load_balancer_controller" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.lbc_role.arn  # Annotate ServiceAccount with IAM role ARN
+    }
+  }
+}
+
 resource "helm_release" "aws_load_balancer_controller" {
   name       = "aws-load-balancer-controller"
   namespace  = "kube-system"
