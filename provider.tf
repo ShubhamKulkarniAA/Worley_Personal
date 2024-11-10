9e3c3a2df48fe1e3b4bb4b11df787f9ad1275d5f
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-11-11 02:43:41 +0530

    Final AWS-LBC -> region ap-south-1

--- provider.tf
+++ provider.tf
@@ -21,24 +21,3 @@ provider "tls" {}
 
 # Fetch the AWS account ID using data source
 data "aws_caller_identity" "current" {}
-
-# Fetch the authentication token for the EKS cluster (Ensure EKS cluster exists before this)
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
-
-# Kubernetes provider configuration
-provider "kubernetes" {
-  host                   = module.eks.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-# Helm provider configuration
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
