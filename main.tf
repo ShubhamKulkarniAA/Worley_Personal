46a6c1a88b62eab371e2b7e19ff550235311422a
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-11-11 01:33:52 +0530

    Final AWS-LBC -> region ap-south-1

--- main.tf
+++ main.tf
@@ -64,6 +64,7 @@ module "lbc" {
   node_role_arn       = module.eks.node_role_arn
   vpc_id              = module.vpc.vpc_id
 
+  depends_on = [aws_eks_cluster.eks_cluster]
 }
 
 # Attach the LBC Custom Policy to the Node Role AFTER Node Group is created
