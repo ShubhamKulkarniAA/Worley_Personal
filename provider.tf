36d73d20273c3f753e2131f89e69b77b65c2d6cf
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-11-12 01:34:55 +0530

    LBC

--- provider.tf
+++ provider.tf
@@ -18,15 +18,23 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = output.eks_cluster_endpoint.value
+  cluster_ca_certificate = base64decode(output.eks_cluster_certificate_authority.value)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+  }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    host                   = output.eks_cluster_endpoint.value
+    cluster_ca_certificate = base64decode(output.eks_cluster_certificate_authority.value)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    }
   }
 }
