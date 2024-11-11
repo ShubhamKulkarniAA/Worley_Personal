ba2c8864db2597e410f8d62b2b6890beedd59af1
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-11-11 21:32:33 +0530

    LBC

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -88,22 +88,6 @@ resource "aws_launch_template" "eks_node_launch_template" {
     instance_metadata_tags   = "disabled"  # Disable metadata tags (optional)
   }
 
-  # Base64 encode the user data
-  user_data = base64encode(<<-EOF
-    MIME-Version: 1.0
-    Content-Type: multipart/mixed; boundary="==BOUNDARY=="
-
-    --==BOUNDARY==
-    Content-Type: text/x-shellscript; charset="us-ascii"
-
-    #!/bin/bash
-    TOKEN=$(curl -X PUT -H "X-aws-ec2-metadata-token-ttl-seconds: 21600" \
-    http://169.254.169.254/latest/api/token)
-    echo $TOKEN > /etc/metadata_token
-
-    --==BOUNDARY==--
-  EOF
-  )
 }
 
 # EKS Node Group
@@ -117,12 +101,6 @@ resource "aws_eks_node_group" "eks_node_group" {
     desired_size = var.desired_size
     max_size     = var.max_size
     min_size     = var.min_size
-  }
-
-  # Specify the instance profile in the launch template
-  launch_template {
-    id      = aws_launch_template.eks_node_launch_template.id
-    version = "$Default"
   }
 
   depends_on = [aws_eks_cluster.eks_cluster]
