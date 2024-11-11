f32e31c04683aeb9b4e28efb6fc4e59120c54db4
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-11-11 11:06:03 +0530

    Final AWS-LBC -> region ap-south-1

--- provider.tf
+++ provider.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "terraform-nc-bucket-test"
-    key    = "terraform-nc-bucket-test/terraform.tfstate"
-    region = "ap-southeast-1"
+    bucket = "worley-nc-test-bucket"
+    key    = "worley-nc-test-bucket/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
 
