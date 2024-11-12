9b39111a413c1133c2ddc04b1578ae9ab5539406
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-11-12 23:46:39 +0530

    Remove EKS,LBC Part-1

--- main.tf
+++ main.tf
@@ -29,20 +29,20 @@ module "ecr" {
   tags                   = var.tags
 }
 
-module "eks" {
-  source = "./modules/eks"
+# module "eks" {
+#   source = "./modules/eks"
 
-  cluster_name          = var.cluster_name
-  node_group_name       = var.node_group_name
-  desired_size          = var.desired_size
-  max_size              = var.max_size
-  min_size              = var.min_size
-  ec2_key_name          = var.ec2_key_name
-  instance_type         = var.instance_type
-  subnet_ids            = [module.vpc.public_subnet1_id, module.vpc.public_subnet2_id]
-  cluster_role_arn      = module.eks.cluster_role_arn
-  node_role_arn         = module.eks.node_role_arn
-}
+#   cluster_name          = var.cluster_name
+#   node_group_name       = var.node_group_name
+#   desired_size          = var.desired_size
+#   max_size              = var.max_size
+#   min_size              = var.min_size
+#   ec2_key_name          = var.ec2_key_name
+#   instance_type         = var.instance_type
+#   subnet_ids            = [module.vpc.public_subnet1_id, module.vpc.public_subnet2_id]
+#   cluster_role_arn      = module.eks.cluster_role_arn
+#   node_role_arn         = module.eks.node_role_arn
+# }
 
 ############################ Apply in 2nd Part ###############################
 
