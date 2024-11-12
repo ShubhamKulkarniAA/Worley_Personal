04688b1b8435fe94be50a78f9c04aac4decebcf4
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-11-12 19:13:12 +0530

    EKS,LBC Part-1

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ module "eks" {
 
   cluster_name          = var.cluster_name
   node_group_name       = var.node_group_name
+  region                = module.vpc.region
   desired_size          = var.desired_size
   max_size              = var.max_size
   min_size              = var.min_size
