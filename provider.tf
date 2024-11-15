e4a638610cd9a05be52c1daf7cefeb80ede3c932
Amey <ameyjadhaav@gmail.com>
2024-11-15 21:11:50 +0530

    Part -1

--- provider.tf
+++ provider.tf
@@ -23,15 +23,15 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-data "aws_eks_cluster" "cluster" { name = module.eks.cluster_name }
-data "aws_eks_cluster_auth" "cluster" { name = module.eks.cluster_name }
-
 ####################### Apply in 2nd Part ######################
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
+# data "aws_eks_cluster" "cluster" { name = module.eks.cluster_name }
+# data "aws_eks_cluster_auth" "cluster" { name = module.eks.cluster_name }
+
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.cluster.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.cluster.token
+#   }
+# }
