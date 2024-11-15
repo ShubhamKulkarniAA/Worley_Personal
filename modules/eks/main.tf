c53c34ac3ffbfa4326408d097411e0354c11a75a
Amey <ameyjadhaav@gmail.com>
2024-11-15 20:24:40 +0530

    -lbc

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -105,6 +105,10 @@ resource "aws_eks_node_group" "eks_node_group" {
   depends_on = [aws_eks_cluster.eks_cluster]
 }
 
+data "tls_certificate" "eks_cluster" {
+  url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
 resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.eks_cluster.certificates[0].sha1_fingerprint]
