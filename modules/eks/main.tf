4a2b63034acf23853e215f82043340d4c3ff683b
Amey <ameyjadhaav@gmail.com>
2024-11-15 22:06:54 +0530

    Part -2

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,30 +1,6 @@
-# IAM Role for EKS Cluster
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# Attach the policy to EKS cluster role
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
 # IAM Role for EKS Node
 resource "aws_iam_role" "eks_node_role" {
-  name = "eks-node-role" # Ensure the role name follows valid IAM naming conventions
+  name = "eks-node-role-${var.cluster_name}" # Append the cluster name to the role name
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -55,64 +31,4 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
 resource "aws_iam_role_policy_attachment" "eks_ecr_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_node_role.name
-}
-
-# EKS Cluster definition
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = var.subnet_ids
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy
-  ]
-}
-
-# Define Launch Template for EKS Node Group
-resource "aws_launch_template" "eks_node_launch_template" {
-  name_prefix   = "eks-node-template"
-  instance_type = var.instance_type
-  key_name      = var.ec2_key_name
-
-  metadata_options {
-    http_tokens                 = "required"
-    http_put_response_hop_limit = 2
-    http_endpoint               = "enabled"
-  }
-}
-
-# EKS Node Group
-resource "aws_eks_node_group" "eks_node_group" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = var.node_group_name
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = var.subnet_ids
-
-  scaling_config {
-    desired_size = var.desired_size
-    max_size     = var.max_size
-    min_size     = var.min_size
-  }
-
-  launch_template {
-    id      = aws_launch_template.eks_node_launch_template.id
-    version = "$Latest"
-  }
-
-  depends_on = [aws_eks_cluster.eks_cluster]
-}
-
-# Retrieve the OIDC thumbprint for IAM OIDC Provider setup
-data "tls_certificate" "eks_cluster" {
-  url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
-}
-
-# IAM OIDC Provider for EKS
-resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks_cluster.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
