9af2f63ba43f775e310ac158cae732a10b7ee181
Amey <ameyjadhaav@gmail.com>
2024-11-16 01:28:13 +0530

    Part -1

--- provider.tf
+++ provider.tf
@@ -17,14 +17,14 @@ provider "aws" {
   region = var.region
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+# }
 
-data "aws_eks_cluster" "cluster" { name = module.eks.cluster_name }
-data "aws_eks_cluster_auth" "cluster" { name = module.eks.cluster_name }
+# data "aws_eks_cluster" "cluster" { name = module.eks.cluster_name }
+# data "aws_eks_cluster_auth" "cluster" { name = module.eks.cluster_name }
 
 ####################### Apply in 2nd Part ######################
 
