49a3e34bcd14a5639a00332a558aa5a7f3fb15b6
Amey <ameyjadhaav@gmail.com>
2024-11-15 20:38:05 +0530

    -lbc

--- provider.tf
+++ provider.tf
@@ -19,6 +19,13 @@ provider "aws" {
 
 ########################## Apply in 2nd Part ###############################
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+
 # provider "helm" {
 #   kubernetes {
 #     host                   = data.aws_eks_cluster.cluster.endpoint
