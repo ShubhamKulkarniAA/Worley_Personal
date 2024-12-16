4f36e5bbb63b0d88f15e1d2646c33e5e005e8a25
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-12-16 15:22:01 +0530

    eks

--- output.tf
+++ output.tf
@@ -62,12 +62,7 @@ output "eks_cluster_endpoint" {
 
 output "eks_cluster_oidc_provider" {
   description = "The OIDC provider URL of the EKS cluster."
-  value       = module.eks.cluster_oidc_provider
-}
-
-output "eks_node_group_names" {
-  description = "The names of the EKS node groups."
-  value       = module.eks.node_groups
+  value       = module.eks.cluster_oidc_issuer_url
 }
 
 # LBC Outputs
