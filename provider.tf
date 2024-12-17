1a7d61e81c40d71b18faff4ca1a7899b6f4c19c7
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-12-17 13:39:14 +0530

    eks

--- provider.tf
+++ provider.tf
@@ -1,27 +1,25 @@
-provider "aws" {
-  region = "ap-south-1"
+# Authentication for the Kubernetes provider
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
 }
 
+# AWS provider configuration
+provider "aws" {
+  region = var.region
+}
 
-# Fetch the AWS account ID using data source
-data "aws_caller_identity" "current" {}
-
-
-# Fetch the EKS cluster details
-
+# Kubernetes provider configuration
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-data "aws_eks_cluster" "cluster" { name = module.eks.cluster_name }
-data "aws_eks_cluster_auth" "cluster" { name = module.eks.cluster_name }
-
+# Helm provider configuration
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
