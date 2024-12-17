9cca1f01d01b2f01592e71643651fbf0063ea552
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-12-17 12:56:58 +0530

    eks

--- provider.tf
+++ provider.tf
@@ -21,20 +21,24 @@ terraform {
   }
 }
 
+# Authentication for the Kubernetes provider
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
 }
 
+# AWS provider configuration
 provider "aws" {
   region = var.region
 }
 
+# Kubernetes provider configuration
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
+# Helm provider configuration
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
