9b182be18affc8b6ce8f16ac0bcd65d74213d62b
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-12-18 16:47:41 +0530

    lbc - 4

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -3,22 +3,22 @@ output "cluster_id" {
   value       = module.eks.cluster_id
 }
 
+output "cluster_name" {
+  description = "EKS Cluster Name."
+  value       = module.eks.cluster_name
+}
+
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane."
   value       = module.eks.cluster_endpoint
 }
 
-output "oidc_provider_arn" {
-  description = "OIDC provider ARN for EKS."
-  value       = module.eks.oidc_provider_arn
-}
-
 output "oidc_provider_url" {
   description = "OIDC provider URL for EKS."
-  value       = module.eks.oidc_provider_url
+  value       = module.eks.cluster_oidc_issuer_url
 }
 
-output "cluster_name" {
-  description = "EKS Cluster Name."
-  value       = module.eks.cluster_name
+output "oidc_provider_arn" {
+  description = "OIDC provider ARN for EKS."
+  value       = module.eks.iam_openid_connect_provider_arns[0]
 }
