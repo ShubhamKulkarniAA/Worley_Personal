96c4d5bf37c130378bb8600aa83a5912bdd0a275
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-12-18 15:49:34 +0530

    eks - 3

--- provider.tf
+++ provider.tf
@@ -14,4 +14,14 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket = "my-tfstate-bucket-name"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+resource "aws_s3_bucket" "tfstate_bucket" {
+  bucket = "my-tfstate-bucket-name"
+  region = "ap-south-1"
 }
