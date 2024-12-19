1ff4ba14c0144a42791005c149bc78cb3a908b2b
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-12-19 14:21:13 +0530

    Eks + lbc -3

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.8"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -9,17 +9,9 @@ terraform {
       source  = "hashicorp/helm"
       version = "2.13.0"
     }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">=2.7.1"
+    }
   }
 }
