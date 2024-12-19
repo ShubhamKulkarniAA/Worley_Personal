647c0349fe9f7ffdb5c54b15a1033c3ce80b6df9
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-12-19 14:55:48 +0530

    eks + lbc - 4

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,9 +11,10 @@ terraform {
   }
 }
 
+# Provider for Kubernetes
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = module.eks.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
