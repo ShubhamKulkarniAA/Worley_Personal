fcfeaf81b7e18ca71f1ea4cbb04119978c8e39f2
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2024-12-19 11:34:17 +0530

    lbc - 1

--- provider.tf
+++ provider.tf
@@ -18,8 +18,8 @@ provider "aws" {
 # Helm provider for installing Loadbalancer controller.
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    host                   = module.eks.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate_authority)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--profile", "labs", "--cluster-name", module.eks.cluster_name]
